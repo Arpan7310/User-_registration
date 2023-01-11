aa162085d9db44af9f7a3bee8907255908a9047b
Arpan Mukherjee <arpanchrome@gmail.com>
2023-01-11 11:13:24 +0530

    1st commit

---
+++ api/users/user.router.js
@@ -0,0 +1,16 @@
+const  {createUser,getUsers,getUsersById,updateUsers,deleteUser,login}=require('./user.controller');
+
+
+const router=require('express').Router();
+
+
+const {checkToken}=require('../../auth/token_validation');
+
+
+router.post('/',checkToken,createUser);
+router.get('/',checkToken,getUsers);
+router.get('/:id',checkToken,getUsersById);
+router.patch('/',checkToken,updateUsers);
+router.delete('/',checkToken,deleteUser);
+router.post("/login",login);
+module.exports=router;
